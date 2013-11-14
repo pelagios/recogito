9a71b2065d323ed1f099844e59daa83bec7344cd
Rainer Simon <rainer.simon@ait.ac.at>
2013-11-14 12:28:24 +0100

    Initial import

---
+++ js/app.js
@@ -0,0 +1,227 @@
+/** Ensure namespaces **/
+var pelagios = (window.pelagios) ? window.pelagios : { };
+pelagios.georesolution = (pelagios.georesolution) ? pelagios.georesolution : { };
+pelagios.georesolution.widgets = (pelagios.georesolution.widgets) ? pelagios.georesolution.widgets : { };
+
+pelagios.georesolution.CorrectionTool = function(tableDiv, mapDiv) {
+  
+  // TODO for testing only
+  var dataURL = 'test/mock-data.json';
+
+  var grid = this._initTable(tableDiv);
+
+  var map = this._initMap(mapDiv);
+
+  $.getJSON(dataURL, function(data) {
+    // Flatten & repackage response
+    var places = [];
+    $.each(data.parts, function(idx, part) {
+      var sourceURI = part.source;
+      $.each(part.places, function(idx, place) {
+        place.source = sourceURI;
+        places.push(place);
+      });
+    });
+    
+    grid.setData(places, true);
+    grid.render();
+  });
+}
+
+/**
+ * Initializes the SlickGrid table.
+ * @private
+ */
+pelagios.georesolution.CorrectionTool.prototype._initTable = function(tableDiv) {
+  // Custom formatter for Pleiades URIs
+  var formatter = function (row, cell, value, columnDef, dataContext) {
+    if (value) {
+      if (value.indexOf('http://pleiades.stoa.org') == 0) {
+        var id =  value.substring(32);
+        if (id.indexOf('#') > -1)
+          id = id.substring(0, id.indexOf('#'));
+          
+        return '<a href="' + value + '" target="_blank">pleiades:' + id + '</a>';
+      } else {
+        return value;
+      }
+    }
+  }
+
+  var columns = [{ name: "Toponym", field: "toponym", id: "toponym" },
+                 { name: "Place ID", field: "gazetteer_uri", id: "gazetteer_uri" , formatter: formatter },
+                 { name: "Corrected", field: "gazetteer_uri_fixed", id: "gazetteer_uri_fixed", formatter: formatter }];
+
+  var options = { enableCellNavigation: true, enableColumnReorder: false, forceFitColumns: true, autoEdit: false };
+    
+  var grid = new Slick.Grid('#table', {}, columns, options);
+  grid.setSelectionModel(new Slick.RowSelectionModel());
+  grid.onDblClick.subscribe(function(e, args) {
+    // TODO implement
+  });
+
+  // Redraw grid in case of window resize
+  $(window).resize(function() {
+    grid.resizeCanvas();
+  })
+  
+  return grid;
+}
+
+pelagios.georesolution.CorrectionTool.prototype._initMap = function(mapDiv) {
+  var baseLayer = L.tileLayer('http://pelagios.org/tilesets/imperium//{z}/{x}/{y}.png', {
+    attribution: 'Tiles: <a href="http://pelagios.org/maps/greco-roman/about.html">Pelagios</a>, 2012; Data: NASA, OSM, Pleiades, DARMC'
+  });
+        
+  var map = new L.Map(mapDiv, {
+    center: new L.LatLng(41.893588, 12.488022),
+    zoom: 5,
+    layers: [baseLayer],
+    minZoom: 3,
+    maxZoom: 11
+  });
+  
+  return map;
+}
+
+/*
+      $(function() {
+        var findRowsWithGID = function(grid, gazetteerID) {
+          // TODO we could optimize this using an index, but individual EGDs should be small enough
+          var size = grid.getDataLength();
+          var rows = [];
+          for (var i = 0; i < size; i++) {
+            var row = grid.getDataItem(i);
+            if (row.gazetteer_uri == gazetteerID)
+              rows.push(i);
+          }
+          return rows;
+        };
+        
+        var getNextN = function(places, idx, n) {
+          if (!n)
+            n = 2;
+            
+          var nextN = [];
+          var ctr = 1;
+          while (nextN.length < n) {
+            if (places.length <= idx + ctr)
+              break;
+              
+            if (places[idx + ctr].marker)
+              nextN.push(places[idx + ctr]);
+              
+            ctr++;
+          }
+          
+          return nextN;
+        };
+        
+        var getPrevN = function(places, idx, n) {
+          if (!n)
+            n = 2;
+            
+          var prevN = [];
+          var ctr = 1;
+          while (prevN.length < n) {
+            if (idx - ctr < 0)
+              break;
+              
+            if (places[idx - ctr].marker)
+              prevN.push(places[idx - ctr]);
+              
+            ctr++;
+          }
+          
+          return prevN;
+        };
+        
+        var markerStyle = { radius:5, stroke: false, fillOpacity: 0.8 }
+        var markerStyleCorrected = { radius:5, stroke: false, fillOpacity: 0.8, color: 'yellow' } 
+        
+        /** Sequence polylines **
+        var sequence = [];
+        
+        /** Connecting line between a place and a manual fix **
+        var lastFixConnection = undefined;
+        
+        $.getJSON('../api/0Avh0MASZtid1dFQyUFF4TDVmeHVBQVNRUHQ2ZFdIdXc', function(data) {
+        
+          $.each(places, function(idx, place) {
+            if (place.coordinate) {
+              place.marker = L.circleMarker(place.coordinate, markerStyle);
+              place.marker.addTo(map); 
+              place.marker.on('click', function(e) {
+                place.marker.bindPopup(place.toponym + ' (<a href="' + place.source + '">Source</a>)').openPopup(); 
+                var rowsToSelect = findRowsWithGID(grid, place.gazetteer_uri);
+                if (rowsToSelect.length > 0) {
+                  grid.setSelectedRows(rowsToSelect);
+                  grid.scrollRowIntoView(rowsToSelect[0], true);
+                }
+              });
+            }
+            
+            // TODO eliminate code duplication
+            if (place.fixedCoordinate) {
+              place.marker = L.circleMarker(place.fixedCoordinate, markerStyleCorrected);
+              place.marker.addTo(map); 
+              place.marker.on('click', function(e) {
+                place.marker.bindPopup(place.toponym + ' (<a href="' + place.source + '">Source</a>)').openPopup(); 
+
+                if (lastFixConnection) {
+                    map.removeLayer(lastFixConnection);
+                    lastFixConnection = undefined;
+                }
+
+                if (place.coordinate) {
+                  var connection = [ place.coordinate, place.fixedCoordinate ];
+                  lastFixConnection = L.polyline(connection, { color: 'yellow', opacity: 1 });
+                  lastFixConnection.addTo(map);
+                }
+              });
+            }
+          });
+                              
+          grid.onSelectedRowsChanged.subscribe(function(e, args) { 
+            if (args.rows.length > 0) {
+              var place = places[args.rows[0]]
+              if (place.marker) {
+                map.panTo(place.coordinate);
+                place.marker.bindPopup(place.toponym + ' (<a href="' + place.source + '">Source</a>)').openPopup();
+                
+                // Clear sequence polylines
+                for (idx in sequence) {
+                  map.removeLayer(sequence[idx]);
+                }
+                sequence = [];
+                
+                var next2 = getNextN(places, args.rows[0], 2);
+                var lineNext = [place.coordinate];
+                for (idx in next2) {
+                  var p = next2[idx];
+                  lineNext.push(p.coordinate);
+                }
+                
+                var pLineNext = L.polyline(lineNext, { color: '#00ff00', opacity: 1 });
+                sequence.push(pLineNext);
+                pLineNext.addTo(map);
+                
+                var prev2 = getPrevN(places, args.rows[0], 2);
+                var linePrev = [place.coordinate];
+                for (idx in prev2) {
+                  var p = prev2[idx];
+                  linePrev.push(p.coordinate);
+                }
+                
+                var pLinePrev = L.polyline(linePrev, { color: '#ff0000', opacity: 1 });
+                sequence.push(pLinePrev);
+                pLinePrev.addTo(map);
+                                                
+              } else {
+                alert(place.toponym + ' does not have coordinates.');
+              }
+            }    
+          });
+        });
+      });
+*/
---
+++ js/correction-popup.js
@@ -0,0 +1,64 @@
+/** Ensure namespaces **/
+var pelagios = (window.pelagios) ? window.pelagios : { };
+
+pelagios.tools = (pelagios.tools) ? pelagios.tools : { };
+
+/**
+ * @param {Object} place the place
+ * @constructor
+ */
+pelagios.tools.FixGeoResolutionWidget = function(parentEl, place) {
+  /** @private **/
+  this._el = parentEl;
+  
+  /** @private **/
+  this._place = place;
+
+  this._initInfo(parentEl, this._initMap(parentEl), place);  
+}
+
+pelagios.tools.FixGeoResolutionWidget.prototype._initInfo = function(parentEl, map, place) {
+  var h1 = document.createElement('h1');
+  h1.innerHTML = place.toponym;
+  parentEl.appendChild(h1);
+  
+  if (place.coordinate)
+    L.circleMarker(place.coordinate).addTo(map); 
+  
+  var p = document.createElement('p');
+  p.innerHTML = '<a href="' + place.gazetteer_uri + '">' + place.gazetteer_uri + '</a>';
+  parentEl.appendChild(p);
+  
+  $.getJSON('../search/' + place.toponym.toLowerCase(), function(data) {
+    var searchResults = data.results.length + ' results: <br/>';
+    $.each(data.results, function(idx, place) {
+      if (place.coords)
+        L.circleMarker(place.coords).addTo(map); 
+      
+      searchResults += place.title + ' (' + place.names + ')<br/>';
+    });
+    
+    p.innerHTML = p.innerHTML + '<br/>' + searchResults;
+  });
+}
+
+pelagios.tools.FixGeoResolutionWidget.prototype._initMap = function(parentEl) {
+  var mapDiv = document.createElement('div');
+  mapDiv.className = 'detail-map';
+  parentEl.appendChild(mapDiv);
+  
+  var baseLayer = L.tileLayer('http://pelagios.org/tilesets/imperium//{z}/{x}/{y}.png', {
+        attribution: 'Tiles: <a href="http://pelagios.org/maps/greco-roman/about.html">Pelagios</a>, 2012; Data: NASA, OSM, Pleiades, DARMC'
+      }),
+  
+      map = new L.Map(mapDiv, {
+        center: new L.LatLng(41.893588, 12.488022), // TODO fit to place coords
+        zoom: 3,
+        layers: [baseLayer],
+        minZoom: 3,
+        maxZoom: 11
+      });
+
+  return map;
+}
+
