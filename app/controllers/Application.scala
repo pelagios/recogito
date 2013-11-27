931fab1434e1b26fbecca4b0c073c34b1079495b
Rainer Simon <rainer.simon@ait.ac.at>
2013-11-27 16:12:27 +0100

    Status bar

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,6 +6,6 @@ import play.api.mvc.{ Action, Controller }
 object Application extends Controller with Secured {
   
   /** Returns the index HTML page for logged-in users **/
-  def index() = withAuth { username => implicit request => Ok(views.html.index()) }
+  def index() = withAuth { username => implicit request => Ok(views.html.index(username)) }
 
 }
--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -1,12 +1,12 @@
 pelagios.georesolution.Footer = function(footerDiv) {
-  this.element = footerDiv;
+  this.element = $(footerDiv).find('#footer-info');
 }
 
 pelagios.georesolution.Footer.prototype.setData = function(data) {
   var verified = $.grep(data, function(annotation, idx) { return annotation.status == 'VERIFIED'; })
   var verifiedPercent = verified.length / data.length;
-  this.element.innerHTML = 
-    data.length + ' Annotations. ' + 
-    verified.length + ' Verified - ' + 
-    (verifiedPercent * 100).toFixed(1) + '% (<a href="logout">Logout</a>).';
+  $(this.element).html(
+    data.length + ' Annotations - ' + 
+    verified.length + ' Verified (' + 
+    (verifiedPercent * 100).toFixed(1) + '%)');
 }
