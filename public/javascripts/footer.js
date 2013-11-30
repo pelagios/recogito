e58442d3da753a6dfd3f8e5bcf9ea8bd202db4c5
Rainer Simon <rainer.simon@ait.ac.at>
2013-11-30 21:01:29 +0100

    Updated footer stats

--- public/javascripts/footer.js
+++ public/javascripts/footer.js
@@ -3,10 +3,24 @@ pelagios.georesolution.Footer = function(footerDiv) {
 }
 
 pelagios.georesolution.Footer.prototype.setData = function(data) {
-  var verified = $.grep(data, function(annotation, idx) { return annotation.status == 'VERIFIED'; })
-  var verifiedPercent = verified.length / data.length;
+  var count = function(status) {
+    var list = $.grep(data, function(annotation, idx) { return annotation.status == status; });
+    return list.length;
+  }
+  
+  var total = data.length;
+  var verified = count('VERIFIED');
+  var not_identifyable = count('NOT_IDENTIFYABLE');
+  var false_detection = count('FALSE_DETECTION');
+  var ignore = count('IGNORE');
+  var complete = verified / (total - not_identifyable - false_detection - ignore);
+  
   $(this.element).html(
-    data.length + ' Annotations - ' + 
-    verified.length + ' Verified (' + 
-    (verifiedPercent * 100).toFixed(1) + '%)');
+    data.length + ' Annotations &nbsp; ' + 
+    '<span class="icon">&#xf14a;</span> ' + verified + ' &nbsp; ' + 
+    '<span class="icon">&#xf024;</span> ' + not_identifyable + ' &nbsp; ' + 
+    '<span class="icon">&#xf057;</span> ' + false_detection + ' &nbsp;' +
+    '<span class="icon">&#xf05e;</span> ' + ignore + ' &nbsp; - &nbsp; ' + 
+
+    (complete * 100).toFixed(1) + '% Complete');
 }
