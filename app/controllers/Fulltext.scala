6e425ce171f46230f3ba7f9179cc7b5684c738b0
Rainer Simon <rainer.simon@ait.ac.at>
2013-12-08 16:05:50 +0100

    Fulltext preview example

--- app/controllers/Fulltext.scala
+++ app/controllers/Fulltext.scala
@@ -3,6 +3,7 @@ package controllers
 import play.api.db.slick._
 import play.api.mvc.Controller
 import models.{ Annotations, GeoDocumentTexts }
+import models.AnnotationStatus
 
 object Fulltext extends Controller with Secured {
   
@@ -11,11 +12,19 @@ object Fulltext extends Controller with Secured {
     val annotations = Annotations.findByGeoDocumentPart(text.gdocPartId.get)
     val string = new String(text.text, "ISO-8859-1")
     
-    val ranges = annotations.map(annotation => {
-      (annotation.offset.get, annotation.toponym.get.size)
-    })
+    val ranges = annotations.foldLeft(("", 0)) { case ((result, beginIndex), annotation) => {
+      val offset = annotation.offset
+      val toponym = annotation.toponym
+      val color = if (annotation.status == AnnotationStatus.VERIFIED) "#77ff77;" else "#aaa;"
+      if (offset.isDefined && toponym.isDefined) {
+        val rs = result + string.substring(beginIndex, offset.get) + "<span style=\"background-color:" + color + "\">" + toponym.get + "</span>"
+        (rs, offset.get + toponym.get.size)
+      } else {
+        (result, beginIndex)
+      }
+    }}
     
-    Ok(views.html.fulltext(string))
+    Ok(views.html.fulltext(ranges._1.replace("\n", "<br/>")))
   }
   
 }
