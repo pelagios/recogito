93169b6263d0bb8f19c371be16aedb923b945f9a
Rainer Simon <rainer.simon@ait.ac.at>
2013-12-13 11:24:41 +0100

    First version of functional UI code for create/update/delete of annotations

--- public/javascripts/fulltext.js
+++ public/javascripts/fulltext.js
@@ -19,14 +19,86 @@ pelagios.georesolution.FulltextAnnotationView = function(textDiv) {
   this._EDITOR_TEMPLATE = 
     '<div class="annotation-editor">' + 
     '  <div class="annotation-editor-header"></div>' +
-    '  <div class="annotation-editor-selection"></div>' +
-    '  <button class="annotation-editor-button button-ok">OK</button>' +
-    '  <button class="annotation-editor-button button-cancel">Cancel</button>' +
+    '  <div class="annotation-editor-body">' +
+    '    <span class="annotation-editor-selection"></span>' +
+    '    <span class="annotation-editor-message"></span>' +
+    '  </div>' +
+    '  <div class="annotation-editor-buttons">' +
+    '    <button class="annotation-editor-button button-ok">OK</button>' +
+    '    <button class="annotation-editor-button button-cancel">Cancel</button>' +
+    '  </div>' +
     '<div>';
     
   this._editor;
     
   rangy.init();
+  
+  var wrapToponym = function(selectedRange) { 
+    var span = document.createElement('span');
+    span.className = 'annotation';
+    span.appendChild(document.createTextNode(selectedRange.toString()));
+    selectedRange.deleteContents();
+    selectedRange.insertNode(span);
+  };
+  
+  var unwrapToponym = function(span) {
+    var text = span.text();
+    span.replaceWith(text);
+  };
+  
+  var rewrapToponym = function(selectedRange) {
+    // We'll need to replace all nodes that are fully or partially in the range
+    var nodes = selectedRange.getNodes();
+    
+    // If the selected text ends in an annotation, well' remove that as well
+    if (nodes.length > 1 && nodes[nodes.length-1].parentNode.nodeName == 'SPAN')
+      nodes.push(nodes[nodes.length-1].parentNode);
+
+    // The plaintext within all nodes in the range   
+    var text = ''; 
+    $.each(nodes, function(idx, node) {
+      if (node.nodeType == 3) // Text node
+        text += $(node).text();
+    });
+
+    // Head - all text up to the selected toponym
+    var head = text.substr(0, selectedRange.startOffset);
+    
+    // Toponym
+    var toponym = selectedRange.toString();
+    
+    // Tail - everything after the toponym
+    var tail = text.substr(selectedRange.startOffset + toponym.length);
+
+    // Get the correct anchor node to replace
+    var nodeToReplace;
+    if (nodes[0].parentNode.nodeName == 'SPAN') {
+      nodeToReplace = nodes[0].parentNode;
+      $.each(nodes, function(idx, node) { $(node).remove(); });
+    } else { 
+      nodeToReplace = nodes[0];
+      $.each(nodes, function(idx, node) { 
+        if (idx > 0)
+          $(node).remove(); 
+      });
+    }
+    
+    $(nodeToReplace).replaceWith(head + '<span class="annotation">' + toponym + '</span>' + tail);
+  }
+  
+  // API call - delete annotation
+  var deleteAnnotation = function(id) {
+    
+  };
+  
+  // API call - create new annotation
+  var createAnnotation = function(toponym, offset) {
+    
+  };
+  
+  // API call - update annotation
+  var updateAnnotation = function(id, toponym, offset) {
+  };
  
   var handleSelection = function(e) {  
     var x = e.offsetX,
@@ -52,45 +124,47 @@ pelagios.georesolution.FulltextAnnotationView = function(textDiv) {
         // No span boundaries crossed
         var parent = $(selectedRange.getNodes([3])).parent().filter('span');
         if (parent.length > 0) {
-          // Selection inside a <span> - size-reduction of existing annotation
           var id = getId(parent[0]);
           
           if ($(parent).text() == toponym) {
             // Selection identical with existing annotation - ask if delete?
-            console.log('DELETE ANNOTATION ' + getId(parent) + '?');
+            self.openEditor("MODIFY ANNOTATION", toponym, "Already marked as a toponym. Do you want to delete annotation instead?", x, y,
+              function() {
+                deleteAnnotation(getId(parent));
+                unwrapToponym(parent);
+              }
+            );
           } else {
-            // console.log('RESIZE OF ANNOTATION ' + id);
-            self.openEditor("MODIFY ANNOTATION", x, y);
+            self.openEditor("MODIFY ANNOTATION", toponym, "Update toponym?", x, y, function() {
+              updateAnnotation(getId(parent), toponym, offset);
+              rewrapToponym(selectedRange);
+              selection.removeAllRanges();
+            });
           }
         } else {
           // A new annotation
-          // console.log('NEW ANNOTATION');
-          self.openEditor("CREATE NEW ANNOTATION", x, y);
+          self.openEditor("NEW ANNOTATION", toponym, "Mark as toponym?", x, y, function() {
+            createAnnotation(toponym, offset);
+            wrapToponym(selectedRange);
+          });
         }        
       } else if (spans.length == 1) {
         // One span crossed - resize/reanchoring of an existing annotation
         var id = getId(spans[0]);
-        // console.log('MODIFICATION OF ANNOTATION ' + id);
-        self.openEditor("MODIFY ANNOTATION", x, y);
+        self.openEditor("MODIFY ANNOTATION", toponym, "Update toponym?", x, y, function() {
+          updateAnnotation(id, toponym, offset);
+          rewrapToponym(selectedRange);
+          selection.removeAllRanges();
+        });
       } else {
         // More than one span crossed - merge
         var ids = $.map(spans, function(span) { return getId(span); });
-        // console.log('MERGING ' + ids);
-        self.openEditor("MERGE ANNOTATIONS", x, y);
+        self.openEditor("MERGE ANNOTATIONS", toponym, "Merge to one toponym?", x, y, function() {
+          // TODO
+          rewrapToponym(selectedRange);
+          selection.removeAllRanges();
+        });
       }
-      
-      // TODO confirmation dialog
-      
-      /* TODO use CSS class rather than hard-coded color
-      var highlight = document.createElement('span');
-      highlight.style.backgroundColor = '#aaa';
-      highlight.appendChild(document.createTextNode(toponym));
-      
-      selectedRange.deleteContents();
-      selectedRange.insertNode(highlight);
-      */
-      
-      console.log('Toponym: ' + toponym + ' - ' + offset); 
     }
   };
     
@@ -99,16 +173,18 @@ pelagios.georesolution.FulltextAnnotationView = function(textDiv) {
   });
 }
 
-pelagios.georesolution.FulltextAnnotationView.prototype.openEditor = function(msg, x, y) {  
+pelagios.georesolution.FulltextAnnotationView.prototype.openEditor = function(title, selection, msg, x, y, ok_callback) {  
   this._editor = $(this._EDITOR_TEMPLATE);
   
   var self = this,
       e = $(this._editor);
-  e.find('.annotation-editor-header').html(msg);
+  e.find('.annotation-editor-header').html(title);
+  e.find('.annotation-editor-selection').html(selection);
+  e.find('.annotation-editor-message').html(msg);
   e.appendTo(document.body);
   e.css('top', y + 'px');
   e.css('left', x + 'px');  
-  e.find('.button-ok').click(function() { self.closeEditor(); });
+  e.find('.button-ok').click(function() { ok_callback(); self.closeEditor(); });
   e.find('.button-cancel').click(function() { self.closeEditor(); });
 }
 
