e4ff2a9c5287772949075145bb0ecde8fc05d411
Rainer Simon <rainer.simon@ait.ac.at>
2014-01-14 16:04:00 +0100

    Made ingest more flexible

--- app/controllers/io/CSVParser.scala
+++ app/controllers/io/CSVParser.scala
@@ -3,6 +3,7 @@ package controllers.io
 import models._
 import play.api.db.slick._
 import scala.io.Source
+import play.api.Logger
 
 /** Utility object to convert CSV input data to Annotation objects.
   * 
@@ -10,7 +11,7 @@ import scala.io.Source
   */
 object CSVParser {
   
-  private val SEPARATOR = ";"
+  private val SEPARATOR = "\t"
   
   /** Parses an input CSV file and produces annotations.
     * 
@@ -22,9 +23,14 @@ object CSVParser {
   def parse(file: String, gdocId: Int)(implicit s: Session): Seq[Annotation] = {
     val data = Source.fromFile(file).getLines    
     val header = data.take(1).toSeq.head.split(SEPARATOR, -1).toSeq 
-
+    
+    header.foreach(println(_))
+    
     // Helper method to find the row index for a specific label
-    def idx(label: String): Int = header.indexWhere(_.equalsIgnoreCase(label))
+    def idx(label: String): Option[Int] = header.indexWhere(_.equalsIgnoreCase(label)) match {
+      case -1 => None
+      case idx => Some(idx)
+    }
     
     val idxGdocPart = idx("gdoc_part")
     val idxStatus = idx("status")
@@ -36,23 +42,35 @@ object CSVParser {
     val idxCorrectedGazetteerURI = idx("gazetteer_uri_corrected")
     val idxTags = idx("tags")
     val idxComment = idx("comment")
+        
+    // Helper function to turn optional fields to Option[String]
+    def parseOptCol(idx: Option[Int])(implicit fields: Array[String]): Option[String] = {
+      if (idx.isDefined) {
+        val string = fields(idx.get)
+        if (string.trim.isEmpty) 
+          None // The field is in the CSV, but the string is empty -> None 
+        else
+          Some(string) // Field is there & contains a string
+      } else {
+        // If the field is not in the CSV at all -> None
+        None
+      }
+    }
     
-    implicit def toOption(string: String) = if (string.trim.isEmpty) None else Some(string)
-    
-    data.map(_.split(SEPARATOR, -1)).map(fields => {
+    data.map(_.split(SEPARATOR, -1)).map(implicit fields => {
       Annotation(
           None,
           gdocId,
-          GeoDocumentParts.getId(fields(idxGdocPart)),
-          AnnotationStatus.withName(fields(idxStatus)),
-          fields(idxToponym),
-          toOption(fields(idxOffset)).map(_.toInt),
-          fields(idxGazetteerURI),
-          fields(idxCorrectedToponym),
-          toOption(fields(idxCorrectedOffset)).map(_.toInt),
-          fields(idxCorrectedGazetteerURI),
-          fields(idxTags),
-          fields(idxComment))
+          GeoDocumentParts.getId(fields(idxGdocPart.get)), // Note: we strictly require gdoc_part to be set by convention!
+          parseOptCol(idxStatus).map(AnnotationStatus.withName(_)).getOrElse(AnnotationStatus.NOT_VERIFIED),
+          parseOptCol(idxToponym),
+          parseOptCol(idxOffset).map(_.toInt),
+          parseOptCol(idxGazetteerURI),
+          parseOptCol(idxCorrectedToponym),
+          parseOptCol(idxCorrectedOffset).map(_.toInt),
+          parseOptCol(idxCorrectedGazetteerURI),
+          parseOptCol(idxTags),
+          parseOptCol(idxComment))
     }).toSeq
   }
   
