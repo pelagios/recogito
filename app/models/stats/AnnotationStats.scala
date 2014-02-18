9fd289c5fd81789dc34bde2f594102c23f0daf14
Rainer Simon <rainer.simon@ait.ac.at>
2014-02-18 10:48:26 +0100

    Diagnostics logging

--- app/models/stats/AnnotationStats.scala
+++ app/models/stats/AnnotationStats.scala
@@ -4,6 +4,7 @@ import global.Global
 import models.Annotation
 import org.pelagios.api.{ Place, PlaceCategory }
 import org.pelagios.gazetteer.GazetteerUtils
+import play.api.Logger
 
 object AnnotationStats {
   
@@ -18,8 +19,14 @@ object AnnotationStats {
       .map { case (uri, annotations) => (uri.get, annotations.size) }.toSeq // Map to (uri -> no. of occurrences)
       
     // Map from (uri -> occurrences) to (place -> occurrences)
-    uniqueURIs.map(tuple => (Global.index.findByURI(tuple._1), tuple._2)) 
-      .map(tuple => (tuple._1.get, tuple._2)) // We should never have any undefined URIs in practice - if we do: fail early, fail often!
+    val uniquePlaces = uniqueURIs.map(tuple => (tuple._1, Global.index.findByURI(tuple._1), tuple._2))
+    
+    val integrityViolations = uniquePlaces.filter(_._2.isEmpty)
+    integrityViolations.foreach(violation => {
+      Logger.warn("Annotation contains invalid URI: " + violation._1)
+    })
+    
+    uniquePlaces.map(tuple => (tuple._2.get, tuple._3)) // We should never have any undefined URIs in practice - if we do: fail early, fail often!
       .sortBy(t => (-t._2, t._1.title))
   }
   
