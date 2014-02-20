01aeb7da7fd3d4fd27b7a1a95e9e85f61dc5cdb2
Rainer Simon <rainer.simon@ait.ac.at>
2014-02-20 15:31:18 +0100

    Extended status Enum

--- app/models/AnnotationStatus.scala
+++ app/models/AnnotationStatus.scala
@@ -8,23 +8,40 @@ import scala.slick.lifted.MappedTypeMapper
   */
 object AnnotationStatus extends Enumeration {
   
+  /** The annotation is not manually verified **/
   val NOT_VERIFIED = Value("NOT_VERIFIED")
   
-  val VERIFIED = Value("VERIFIED")
+  /** The annotation is verified and assumed to be correct **/
+  val VERIFIED = Value("VERIFIED")  
   
-  val NOT_IDENTIFYABLE = Value("NOT_IDENTIFYABLE")
-  
+  /** The annotation was generated automatically, but was a false detection **/ 
   val FALSE_DETECTION = Value ("FALSE_DETECTION")
   
+  /** Ignore this annotation for the purposes of mapping (for whatever reason) **/
   val IGNORE = Value("IGNORE")
-    
+  
+  /** There was no suitable match in the gazetteer for this place **/ 
+  val NO_SUITABLE_MATCH = Value("NO_SUITABLE_MATCH")
+  
+  /** There were multiple gazetteer entries this place may correspond to **/
+  val AMBIGUOUS = Value("AMBIGUOUS")
+  
+  /** The annotation/toponym refers to multiple places at once **/
+  val MULTIPLE = Value("MULTIPLE")
+  
+  /** This place is not identifiable (for whatever reason) **/
+  val NOT_IDENTIFYABLE = Value("NOT_IDENTIFYABLE")
+  
   def screenName(status: AnnotationStatus.Value) = {
     status match {
       case NOT_VERIFIED => "Unverified"
       case VERIFIED => "Verified"
-      case NOT_IDENTIFYABLE => "Unknown, Not Identifyable"
       case FALSE_DETECTION => "False Detection, Deleted"
       case IGNORE => "Ignore"
+      case NO_SUITABLE_MATCH => "No Suitable Gazetteer URI"
+      case AMBIGUOUS => "Ambiguous - Multiple Possible URIs"
+      case MULTIPLE => "Toponym Refers to Multiple Places"
+      case NOT_IDENTIFYABLE => "Unknown, Not Identifyable"
     }
   }
   
