8abea763095411341116f1d0eb39fafb226efae8
Rainer Simon <rainer.simon@ait.ac.at>
2014-03-03 06:47:35 -0500

    OA RDF serialization (work in progress)

--- app/controllers/DocumentController.scala
+++ app/controllers/DocumentController.scala
@@ -6,6 +6,11 @@ import play.api.db.slick._
 import play.api.mvc.{ Action, Controller }
 import play.api.libs.json.{ Json, JsObject }
 import play.api.Play.current
+import org.openrdf.rio.RDFFormat
+import org.pelagios.Scalagios
+import org.pelagios.api.{ Annotation => OAnnotation }
+import java.io.ByteArrayOutputStream
+import org.pelagios.api.AnnotatedThing
 
 /** GeoDocument JSON API.
   *
@@ -14,6 +19,8 @@ import play.api.Play.current
 object DocumentController extends Controller with Secured {
   
   private val CSV = "csv"
+  private val RDF_XML = "rdfxml"
+  private val UTF8 = "UTF-8"
   
   /** Returns the list of all geo documents in the database as JSON **/
   def listAll = DBAction { implicit session =>
@@ -33,34 +40,48 @@ object DocumentController extends Controller with Secured {
     * @param the format
     */
   def get(id: Int, format: Option[String]) = DBAction { implicit session =>
-    if (format.isDefined && format.get.equalsIgnoreCase(CSV))
-      get_CSV(id)
-    else
-      get_JSON(id)
-  }
-  
-  private def get_CSV(id: Int)(implicit session: Session) = {
     val doc = GeoDocuments.findById(id)
     if (doc.isDefined) {
-      val annotations = Annotations.findByGeoDocumentAndStatus(id, AnnotationStatus.VERIFIED)
-      val serializer = new CSVSerializer()
-      Ok(serializer.asConsolidatedResult(annotations)).withHeaders(CONTENT_TYPE -> "text/csv", CONTENT_DISPOSITION -> ("attachment; filename=pelagios-egd-" + id.toString + ".csv"))
+      if (format.isDefined && format.get.equalsIgnoreCase(CSV))
+        get_CSV(doc.get)
+      else if (format.isDefined && format.get.equalsIgnoreCase(RDF_XML))
+        get_RDF(doc.get, RDFFormat.RDFXML)
+      else
+        get_JSON(doc.get)
     } else {
       val msg = "No document with ID " + id
-      NotFound(Json.obj("error" -> msg))      
+      NotFound(Json.obj("error" -> msg))            
     }
-       
+  }
+  
+  private def get_CSV(doc: GeoDocument)(implicit session: Session) = {
+    val id = doc.id.get
+    val annotations = Annotations.findByGeoDocumentAndStatus(id, AnnotationStatus.VERIFIED)
+    val serializer = new CSVSerializer()
+    Ok(serializer.asConsolidatedResult(annotations)).withHeaders(CONTENT_TYPE -> "text/csv", CONTENT_DISPOSITION -> ("attachment; filename=pelagios-egd-" + id.toString + ".csv"))  
+  }
+  
+  private def get_RDF(doc: GeoDocument, format: RDFFormat)(implicit session: Session) = {
+    // TODO construct URI based on Recogito base URL (somehow...)
+    val thing = AnnotatedThing("http://www.example.org/pelagios/egd", doc.title)
+    val annotations = Annotations.findByGeoDocumentAndStatus(doc.id.get, AnnotationStatus.VERIFIED)
+    
+    // Convert Recogito annotations to OA
+    annotations.zipWithIndex.foreach{ case (a, idx) => {
+      val place =  { if (a.correctedGazetteerURI.isDefined) a.correctedGazetteerURI else a.gazetteerURI }
+        .map(Seq(_)).getOrElse(Seq.empty[String])
+        
+      val target = "" // TODO
+      val oa = OAnnotation("http://www.example.org/pelagios/annotations#" + idx, thing, place = place)
+    }}
+
+    val out = new ByteArrayOutputStream()
+    Scalagios.writeAnnotations(Seq(thing), out, format)
+    Ok(new String(out.toString(UTF8))).withHeaders(CONTENT_TYPE -> "application/rdf+xml", CONTENT_DISPOSITION -> ("attachment; filename=pelagios-egd.rdf"))      
   }
       
-  private def get_JSON(id: Int)(implicit s: Session) = {
-    val doc = GeoDocuments.findById(id)
-    if (doc.isDefined) {      
-      Ok(JSONSerializer.toJson(doc.get, true))
-    } else {
-      val msg = "No document with ID " + id
-      NotFound(Json.obj("error" -> msg))
-    }
-  }
+  private def get_JSON(doc: GeoDocument)(implicit s: Session) =
+    Ok(JSONSerializer.toJson(doc, true))
   
   /** Deletes a document (and associated data) from the database **/
   def delete(id: Int) = protectedDBAction(Secure.REJECT) { username => implicit session =>
