0fe46e786d193fd1222f506c93e3ebb6c783faec
Rainer Simon <rainer.simon@ait.ac.at>
2014-03-05 17:26:16 -0500

    Atom feed cleanup

--- app/controllers/FeedController.scala
+++ app/controllers/FeedController.scala
@@ -1,34 +1,28 @@
 package controllers
 
-import play.api.mvc.Controller
-import play.api.db.slick.DBAction
-import play.api.Play.current
-import models.EditHistory
-import play.api.db.slick._
-import models.AnnotationStatus
-import com.sun.syndication.feed.synd.SyndFeedImpl
-import com.sun.syndication.feed.synd.SyndEntryImpl
-import com.sun.syndication.feed.synd.SyndContentImpl
-import scala.collection.JavaConversions._
 import com.sun.syndication.io.WireFeedOutput
+import com.sun.syndication.feed.atom.{ Content, Entry, Feed, Person }
 import java.io.StringWriter
-import java.io.Writer
-import com.sun.syndication.feed.atom.{ Entry, Feed }
 import java.util.Date
-import com.sun.syndication.feed.atom.Person
-import com.sun.syndication.feed.rss.Description
-import com.sun.syndication.feed.atom.Content
+import models.{ AnnotationStatus, EditHistory }
+import play.api.mvc.Controller
+import play.api.db.slick._
+import play.api.Play
+import play.api.Play.current
+import scala.collection.JavaConversions._
 
 object FeedController extends Controller {
   
+  private val NUMBER_OF_ENTRIES = Play.current.configuration.getInt("recogito.feed.entries").getOrElse(20)
+  
+  private val ATOM_1_0 = "atom_1.0"
+  
   def recentVerifications = DBAction { implicit session =>
-    // TODO make configurable
-    val mostRecent = EditHistory.getLastN(20, AnnotationStatus.VERIFIED)
-    
+    val mostRecent = EditHistory.getLastN(NUMBER_OF_ENTRIES, AnnotationStatus.VERIFIED)    
     val baseURL = routes.FeedController.recentVerifications.absoluteURL(false)
     
     val feed = new Feed()
-    feed.setFeedType("atom_1.0")
+    feed.setFeedType(ATOM_1_0)
     feed.setTitle("Recogito - Recent Verifications")
     feed.setId(baseURL)
     feed.setUpdated(new Date(mostRecent.head.timestamp.getTime))
@@ -54,7 +48,7 @@ object FeedController extends Controller {
     feed.setEntries(entries)
     
     val wirefeed = new WireFeedOutput()
-    val writer: Writer = new StringWriter()
+    val writer = new StringWriter()
     wirefeed.output(feed, writer)
     writer.flush()
     writer.close()
