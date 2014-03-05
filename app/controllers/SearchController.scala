5735991dae05b07ef69ddba3207aa88a549e1860
Rainer Simon <rainer.simon@ait.ac.at>
2014-03-05 16:17:25 -0500

    Cleanup

--- app/controllers/SearchController.scala
+++ app/controllers/SearchController.scala
@@ -5,7 +5,6 @@ import play.api.db.slick._
 import play.api.mvc.{ Action, Controller }
 import play.api.libs.json.Json
 import play.api.Play.current
-import models.GeoDocumentTexts
 import models.{ Annotation, Annotations, AnnotationStatus }
 
 /** Toponym search API controller.
@@ -17,20 +16,21 @@ object SearchController extends Controller {
   private val UTF8 = "UTF-8"
   
   private val PLEIADES_PREFIX = "http://pleiades.stoa.org"
+
   private val DARE_PREFIX = "http://www.imperium.ahlfeldt.se/"
     
   def placeSearch(query: String) = Action {
-    // For search, we're restricting to Pleiades URIs only
-    val results = Global.index.query(query, true).filter(result => result.uri.startsWith(PLEIADES_PREFIX) || result.uri.startsWith("http://www.alraqmiyyat.org/")).map(place => { 
-      // We use DARE coordinates if we have them
-      val coordinate = place.map(place => {
+    // A little hard-wired hack for Pleiades and DARE:
+    // We don't want DARE to appear in the results - but we want to use DARE coordinates instead of Pleiades where available
+    val results = Global.index.query(query, true).filter(!_.uri.startsWith(DARE_PREFIX)).map(place => { 
+      val coordinate = {
         val dareEquivalent = Global.index.getNetwork(place).places.filter(_.uri.startsWith(DARE_PREFIX))
         if (dareEquivalent.size > 0) {
           dareEquivalent(0).getCentroid
         } else {
           place.getCentroid
         }
-      }).flatten      
+      }
       
       Json.obj(
         "uri" -> place.uri,
