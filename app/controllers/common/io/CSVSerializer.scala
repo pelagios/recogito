4daf50020e1cbe7f316dbbadc417d8ff8e84b7e9
Rainer Simon <rainer.simon@ait.ac.at>
2014-05-06 11:32:14 +0200

    GDoc part now included in public-facing CSV

--- app/controllers/common/io/CSVSerializer.scala
+++ app/controllers/common/io/CSVSerializer.scala
@@ -13,6 +13,86 @@ class CSVSerializer extends BaseSerializer {
   
   private val SEPARATOR = ";"
     
+  /** Generates 'consolidated output' for public consumption.
+    *
+    * This version of the CSV data exposes *only* the verified annotations,
+    * with just the final, corrected toponyms and gazetteer IDs. Information about
+    * whether data was generated automatically or manually, what corrections
+    * were made, etc. is no longer included in this data. CSV files of this type 
+    * CANNOT be used to restore data in the DB!
+    * @param annotations the annotations
+    * @return the CSV
+    */
+  def serializeAnnotationsConsolidated(annotations: Seq[Annotation])(implicit s: Session): String = {
+    val header = Seq("toponym","gazetteer_uri","lat","lng", "place_category", "document_part", "tags", "source").mkString(SEPARATOR) + SEPARATOR + "\n"
+    annotations.foldLeft(header)((csv, annotation) => {
+      val uri = if (annotation.correctedGazetteerURI.isDefined && !annotation.correctedGazetteerURI.get.isEmpty) annotation.correctedGazetteerURI else annotation.gazetteerURI
+      val toponym = if (annotation.correctedToponym.isDefined) annotation.correctedToponym else annotation.toponym
+      
+      if (uri.isDefined && !uri.get.isEmpty) {
+        val queryResult = CrossGazetteerUtils.getPlace(uri.get)
+        val category = queryResult.map(_._1.category).flatten
+        val coord = queryResult.map(_._2).flatten
+        
+        csv + 
+        esc(toponym.getOrElse("")) + SEPARATOR + 
+        GazetteerUtils.normalizeURI(uri.get) + SEPARATOR + 
+        coord.map(_.y).getOrElse("") + SEPARATOR +
+        coord.map(_.x).getOrElse("") + SEPARATOR +
+        category.map(_.toString).getOrElse("") + SEPARATOR +
+        annotation.gdocPartId.map(getPart(_).map(_.title)).flatten.getOrElse("") + SEPARATOR +
+        { if (annotation.tags.size > 0) "\"" + annotation.tags.mkString(",") + "\"" else "" } + SEPARATOR + 
+        getSourceForAnnotation(annotation).getOrElse("") + SEPARATOR + "\n"
+      } else {
+        csv
+      }
+    })
+  }
+  
+  /** Generates a full backup of annotations, compatible with Recogito's upload mechanism.
+    * 
+    * This version of the CSV data exposes all original fields from the annotations table
+    * in the database. CSV files of this type can be used to restore data in the DB.
+    * @param annotations the annotations
+    * @return the CSV
+    */
+  def serializeAnnotationsAsDBBackup(annotations: Seq[Annotation])(implicit s: Session): String = {
+    val header = Seq("uuid", "gdoc_part", "status", "toponym", "offset", "gazetteer_uri", "latlon", "place_category", "toponym_corrected", 
+                     "offset_corrected", "gazetteer_uri_corrected", "latlon_corrected", "place_category_corrected", "tags", "comment", "source", "see_also")
+                     .mkString(SEPARATOR) + SEPARATOR + "\n"
+      
+    annotations.foldLeft(header)((csv, annotation) => {
+      val queryResultForURI = annotation.gazetteerURI.map(uri => CrossGazetteerUtils.getPlace(uri)).flatten
+      val queryResultForCorrectedURI = annotation.correctedGazetteerURI.map(uri => CrossGazetteerUtils.getPlace(uri)).flatten
+          
+      val placeCategory = queryResultForURI.map(_._1.category).flatten
+      val coordinate = queryResultForURI.map(_._2).flatten
+      
+      val correctedPlaceCategory = queryResultForCorrectedURI.map(_._1.category).flatten
+      val correctedCoordinate = queryResultForCorrectedURI.map(_._2).flatten
+      
+      csv + 
+      annotation.uuid + SEPARATOR +
+      annotation.gdocPartId.map(getPart(_).map(_.title)).flatten.getOrElse("") + SEPARATOR +
+      annotation.status + SEPARATOR +
+      esc(annotation.toponym.getOrElse("")) + SEPARATOR +
+      annotation.offset.getOrElse("") + SEPARATOR +
+      annotation.gazetteerURI.map(GazetteerUtils.normalizeURI(_)).getOrElse("") + SEPARATOR +
+      coordinate.map(c => c.x + "," + c.y).getOrElse("") + SEPARATOR +
+      placeCategory.map(_.toString).getOrElse("") + SEPARATOR +
+      esc(annotation.correctedToponym.getOrElse("")) + SEPARATOR +
+      annotation.correctedOffset.getOrElse("") + SEPARATOR +
+      annotation.correctedGazetteerURI.map(GazetteerUtils.normalizeURI(_)).getOrElse("") + SEPARATOR +
+      correctedCoordinate.map(c => c.x + "," + c.y).getOrElse("") + SEPARATOR +
+      correctedPlaceCategory.map(_.toString).getOrElse("") + SEPARATOR +
+      esc(annotation.tags.getOrElse("")) + SEPARATOR +
+      esc(annotation.comment.getOrElse("")) + SEPARATOR +
+      annotation.source.getOrElse("") + SEPARATOR +
+      annotation.seeAlso.mkString(",") + SEPARATOR +
+      "\n"
+    })
+  }
+  
   /** Serializes the list of users for backup purposes.
     *
     * @param users the users to serialize to CSV  
@@ -69,85 +149,6 @@ class CSVSerializer extends BaseSerializer {
       record.unverifiedToponyms + SEPARATOR +
       record.unidentifiableToponyms + SEPARATOR +
       record.totalEdits + SEPARATOR + "\n"
-    })
-  }
-  
-  /** Generates 'consolidated output' for public consumption.
-    *
-    * This version of the CSV data exposes *only* the verified annotations,
-    * with just the final, corrected toponyms and gazetteer IDs. Information about
-    * whether data was generated automatically or manually, what corrections
-    * were made, etc. is no longer included in this data. CSV files of this type 
-    * CANNOT be used to restore data in the DB!
-    * @param annotations the annotations
-    * @return the CSV
-    */
-  def serializeAnnotationsConsolidated(annotations: Seq[Annotation])(implicit s: Session): String = {
-    val header = Seq("toponym","uri","lat","lng", "place_category", "tags", "source").mkString(SEPARATOR) + SEPARATOR + "\n"
-    annotations.foldLeft(header)((csv, annotation) => {
-      val uri = if (annotation.correctedGazetteerURI.isDefined && !annotation.correctedGazetteerURI.get.isEmpty) annotation.correctedGazetteerURI else annotation.gazetteerURI
-      val toponym = if (annotation.correctedToponym.isDefined) annotation.correctedToponym else annotation.toponym
-      
-      if (uri.isDefined && !uri.get.isEmpty) {
-        val queryResult = CrossGazetteerUtils.getPlace(uri.get)
-        val category = queryResult.map(_._1.category).flatten
-        val coord = queryResult.map(_._2).flatten
-        
-        csv + 
-        esc(toponym.getOrElse("")) + SEPARATOR + 
-        GazetteerUtils.normalizeURI(uri.get) + SEPARATOR + 
-        coord.map(_.y).getOrElse("") + SEPARATOR +
-        coord.map(_.x).getOrElse("") + SEPARATOR +
-        category.map(_.toString).getOrElse("") + SEPARATOR +
-        { if (annotation.tags.size > 0) "\"" + annotation.tags.mkString(",") + "\"" else "" } + SEPARATOR + 
-        getSourceForAnnotation(annotation).getOrElse("") + SEPARATOR + "\n"
-      } else {
-        csv
-      }
-    })
-  }
-  
-  /** Generates a full backup of annotations, compatible with Recogito's upload mechanism.
-    * 
-    * This version of the CSV data exposes all original fields from the annotations table
-    * in the database. CSV files of this type can be used to restore data in the DB.
-    * @param annotations the annotations
-    * @return the CSV
-    */
-  def serializeAnnotationsAsDBBackup(annotations: Seq[Annotation])(implicit s: Session): String = {
-    val header = Seq("uuid", "gdoc_part", "status", "toponym", "offset", "gazetteer_uri", "latlon", "place_category", "toponym_corrected", 
-                     "offset_corrected", "gazetteer_uri_corrected", "latlon_corrected", "place_category_corrected", "tags", "comment", "source", "see_also")
-                     .mkString(SEPARATOR) + SEPARATOR + "\n"
-      
-    annotations.foldLeft(header)((csv, annotation) => {
-      val queryResultForURI = annotation.gazetteerURI.map(uri => CrossGazetteerUtils.getPlace(uri)).flatten
-      val queryResultForCorrectedURI = annotation.correctedGazetteerURI.map(uri => CrossGazetteerUtils.getPlace(uri)).flatten
-          
-      val placeCategory = queryResultForURI.map(_._1.category).flatten
-      val coordinate = queryResultForURI.map(_._2).flatten
-      
-      val correctedPlaceCategory = queryResultForCorrectedURI.map(_._1.category).flatten
-      val correctedCoordinate = queryResultForCorrectedURI.map(_._2).flatten
-      
-      csv + 
-      annotation.uuid + SEPARATOR +
-      annotation.gdocPartId.map(getPart(_).map(_.title)).flatten.getOrElse("") + SEPARATOR +
-      annotation.status + SEPARATOR +
-      esc(annotation.toponym.getOrElse("")) + SEPARATOR +
-      annotation.offset.getOrElse("") + SEPARATOR +
-      annotation.gazetteerURI.map(GazetteerUtils.normalizeURI(_)).getOrElse("") + SEPARATOR +
-      coordinate.map(c => c.x + "," + c.y).getOrElse("") + SEPARATOR +
-      placeCategory.map(_.toString).getOrElse("") + SEPARATOR +
-      esc(annotation.correctedToponym.getOrElse("")) + SEPARATOR +
-      annotation.correctedOffset.getOrElse("") + SEPARATOR +
-      annotation.correctedGazetteerURI.map(GazetteerUtils.normalizeURI(_)).getOrElse("") + SEPARATOR +
-      correctedCoordinate.map(c => c.x + "," + c.y).getOrElse("") + SEPARATOR +
-      correctedPlaceCategory.map(_.toString).getOrElse("") + SEPARATOR +
-      esc(annotation.tags.getOrElse("")) + SEPARATOR +
-      esc(annotation.comment.getOrElse("")) + SEPARATOR +
-      annotation.source.getOrElse("") + SEPARATOR +
-      annotation.seeAlso.mkString(",") + SEPARATOR +
-      "\n"
     })
   }
   
