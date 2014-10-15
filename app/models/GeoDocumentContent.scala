0284dbc3bb94097e87aef45339ddeb23b9c13762
Rainer Simon <rainer.simon@ait.ac.at>
2014-10-15 08:30:20 +0200

    Bugfix

--- app/models/GeoDocumentContent.scala
+++ app/models/GeoDocumentContent.scala
@@ -22,11 +22,11 @@ object GeoDocumentContent {
   def findByGeoDocument(gdocId: Int)(implicit session: Session): Seq[(GeoDocumentContent, Option[String])] = {
     val textQuery = for {
       (text, part) <- GeoDocumentTexts.query.where(_.gdocId === gdocId) leftJoin GeoDocumentParts.query on (_.gdocPartId === _.id)
-    } yield (text, part.title.?, part.sequenceNumber)
+    } yield (text, part.title.?, part.sequenceNumber.?)
     
     val imageQuery = for {
       (image, part) <- GeoDocumentImages.query.where(_.gdocId === gdocId) leftJoin GeoDocumentParts.query on (_.gdocPartId === _.id) 
-    } yield (image, part.title.?, part.sequenceNumber)
+    } yield (image, part.title.?, part.sequenceNumber.?)
     
     val texts: List[(GeoDocumentContent, Option[String])] = textQuery.list.sortBy(_._3).map(t => (t._1, t._2))
     val images: List[(GeoDocumentContent, Option[String])] = imageQuery.list.sortBy(_._3).map(t => (t._1, t._2))
